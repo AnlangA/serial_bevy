8c9a5c670c3a6e818ee103c37b9b64022499b1ed
anlang <2682525840@QQ.COM>
2025-01-04 17:47:33 +0800

    修改界面

--- src/serial_ui/mod.rs
+++ src/serial_ui/mod.rs
@@ -5,11 +5,12 @@ use crate::serial::*;
 use bevy::{
     prelude::*,
     render::camera::RenderTarget,
-    window::{PresentMode, PrimaryWindow, WindowClosing, WindowRef, WindowResolution},
+    window::{PresentMode, WindowClosing, WindowRef, WindowResolution},
 };
 use bevy_egui::{EguiContext, EguiContexts, EguiPlugin, egui};
 use std::sync::MutexGuard;
 use tokio_serial::{DataBits, FlowControl, Parity, StopBits};
+use ui::*;
 
 /// serial ui plugin
 pub struct SerialUiPlugin;
@@ -20,6 +21,7 @@ impl Plugin for SerialUiPlugin {
         app.add_plugins(EguiPlugin)
             .insert_resource(ClearColor(Color::srgb(0.5, 0.5, 0.9)))
             .insert_resource(Flag { flag: true })
+            .insert_resource(Selected::default())
             .add_systems(Startup, ui_init)
             .add_systems(
                 Update,
@@ -35,7 +37,7 @@ impl Plugin for SerialUiPlugin {
 }
 
 /// set theme
-fn ui_init(mut ctx: EguiContexts, mut commands: Commands) {
+fn ui_init(mut ctx: EguiContexts, _commands: Commands) {
     // Start with the default fonts (we will be adding to them rather than replacing thereplacing them).
     let mut fonts = egui::FontDefinitions::default();
 
@@ -65,240 +67,62 @@ fn ui_init(mut ctx: EguiContexts, mut commands: Commands) {
         .push("Song".to_owned());
     // Tell egui to use these fonts:
     ctx.ctx_mut().set_fonts(fonts);
+
+    ctx.ctx_mut().set_theme(egui::Theme::Light);
 }
+
+
 
 /// serial settings ui
-fn serial_ui(mut contexts: EguiContexts, mut serials: Query<&mut Serials>, mut commands: Commands) {
-    for serial in serials.single_mut().serial.iter_mut() {
-        let mut serial = serial.lock().unwrap();
-        egui::Window::new(serial.set.port_name.clone()).show(contexts.ctx_mut(), |ui| {
-            ui.add_enabled_ui(serial.data().state().is_close(), |ui| {
-                draw_baud_rate_selector(ui, &mut serial);
-                draw_data_bits_selector(ui, &mut serial);
-                draw_stop_bits_selector(ui, &mut serial);
-                draw_flow_control_selector(ui, &mut serial);
-                draw_parity_selector(ui, &mut serial);
+fn serial_ui(mut contexts: EguiContexts, mut serials: Query<&mut Serials>, mut commands: Commands, mut selected: ResMut<Selected>) {
+    
+    egui::SidePanel::left("serial_ui")
+        .resizable(false)
+        .min_width(120.0)
+        .max_width(120.0)
+        .show(contexts.ctx_mut(), |ui| {
+            ui.horizontal(|ui| {
+                egui::widgets::global_theme_preference_switch(ui);
             });
-            open_ui(ui, &mut serial, &mut commands);
-        });
-    }
-}
-
-/// draw baud rate selector
-fn draw_baud_rate_selector(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>) {
-    ui.horizontal(|ui| {
-        ui.label("波特率");
-        egui::ComboBox::from_id_salt("波特率")
-            .width(60f32)
-            .selected_text(serial.set.baud_rate().to_string())
-            .show_ui(ui, |ui| {
-                for baud_rate in port::COMMON_BAUD_RATES.iter() {
-                    ui.selectable_value(serial.set.baud_rate(), *baud_rate, baud_rate.to_string())
-                        .on_hover_text("选择正确的波特率");
-                }
-            });
-    });
-}
-
-/// draw data bits selector
-fn draw_data_bits_selector(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>) {
-    ui.horizontal(|ui| {
-        ui.label("数据位");
-        egui::ComboBox::from_id_salt("数据位")
-            .width(60f32)
-            .selected_text(serial.set.data_size().to_string())
-            .show_ui(ui, |ui| {
-                for bits in [
-                    DataBits::Five,
-                    DataBits::Six,
-                    DataBits::Seven,
-                    DataBits::Eight,
-                ] {
-                    ui.selectable_value(serial.set.data_size(), bits, format!("{}", bits));
-                }
-            });
-    });
-}
-
-/// draw stop bits selector
-fn draw_stop_bits_selector(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>) {
-    ui.horizontal(|ui| {
-        ui.label("停止位");
-        egui::ComboBox::from_id_salt("停止位")
-            .width(60f32)
-            .selected_text(serial.set.stop_bits().to_string())
-            .show_ui(ui, |ui| {
-                for bits in [StopBits::One, StopBits::Two] {
-                    ui.selectable_value(serial.set.stop_bits(), bits, format!("{}", bits));
-                }
-            });
-    });
-}
-
-/// draw flow control selector
-fn draw_flow_control_selector(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>) {
-    ui.horizontal(|ui| {
-        ui.label("流控    ");
-        egui::ComboBox::from_id_salt("流控")
-            .width(60f32)
-            .selected_text(serial.set.flow_control().to_string())
-            .show_ui(ui, |ui| {
-                for flow in [
-                    FlowControl::None,
-                    FlowControl::Software,
-                    FlowControl::Hardware,
-                ] {
-                    ui.selectable_value(serial.set.flow_control(), flow, format!("{}", flow));
-                }
-            });
-    });
-}
-
-/// draw parity selector
-fn draw_parity_selector(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>) {
-    ui.horizontal(|ui| {
-        ui.label("校验    ");
-        egui::ComboBox::from_id_salt("校验")
-            .width(60f32)
-            .selected_text(serial.set.parity().to_string())
-            .show_ui(ui, |ui| {
-                for parity in [Parity::None, Parity::Odd, Parity::Even] {
-                    ui.selectable_value(serial.set.parity(), parity, format!("{}", parity));
-                }
-            });
-    });
-}
-
-fn open_ui(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>, commands: &mut Commands) {
-    if serial.is_close() {
-        if ui.button("打开").clicked() {
-            info!("Open port {}", serial.set.port_name);
-            if let Some(tx) = serial.tx_channel() {
-                match tx.send(port::PortChannelData::PortOpen) {
-                    Ok(_) => {
-                        info!("Send open port message");
-                    }
-                    Err(e) => error!("Failed to open port: {}", e),
-                }
-                let time = chrono::Local::now().format("%Y%m%d_%H%M%S").to_string();
-                let port_name = serial.set.port_name.clone();
-                let file_name = format!("{}_{}.txt", port_name, time);
-                serial.data().add_source_file(file_name);
-            }
-        }
-    } else if serial.is_open() {
-        if ui.button("关闭").clicked() {
-            info!("关闭串口 {}", serial.set.port_name);
-            let port_name = serial.set.port_name.clone();
-            match serial.window() {
-                Some(window) => {
-                    commands.entity(window.clone()).despawn_recursive();
-                }
-                None => {}
-            };
-
-            if let Some(tx) = serial.tx_channel() {
-                match tx.send(port::PortChannelData::PortClose(port_name)) {
-                    Ok(_) => {
-                        info!("Send close port message");
-                    }
-                    Err(e) => error!("Failed to close port: {}", e),
-                }
-            }
-        }
-    }
-}
-
-fn close_event_system(
-    mut window_close_events: EventReader<WindowClosing>,
-    mut serials: Query<&mut Serials>,
-) {
-    for event in window_close_events.read() {
-        let mut serial = serials.get_single_mut().unwrap();
-        for serial in serial.serial.iter_mut() {
-            let mut serial = serial.lock().unwrap();
-            let port_name = serial.set.port_name.clone();
-            if serial.is_open() {
-                if let Some(window) = serial.window() {
-                    if *window == event.window {
-                        if let Some(tx) = serial.tx_channel() {
-                            match tx.send(port::PortChannelData::PortClose(port_name)) {
-                                Ok(_) => {
-                                    info!("Send close port message");
-                                }
-                                Err(e) => error!("Failed to close port: {}", e),
-                            }
+            ui.separator();
+            egui::ScrollArea::both().show(ui, |ui| {
+                for serial in serials.single_mut().serial.iter_mut() {
+                    let mut serial = serial.lock().unwrap();
+                    ui.horizontal(|ui| {
+                    if serial.is_open() {
+                        if ui.selectable_label(selected.is_selected(&serial.set.port_name), egui::RichText::new(serial.set.port_name.clone()).color(egui::Color32::ORANGE).strong()).clicked(){
+                            selected.select(&serial.set.port_name);
+                        }
+                    } else {
+                        if ui.selectable_label(selected.is_selected(&serial.set.port_name), egui::RichText::new(serial.set.port_name.clone()).color(egui::Color32::GREEN).strong()).clicked(){
+                            selected.select(&serial.set.port_name);
                         }
                     }
+                    open_ui(ui, &mut serial, &mut commands, &mut selected);
+                });
+                
+
+            }});
+            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+                for serial in serials.single_mut().serial.iter_mut() {
+                    let mut serial = serial.lock().unwrap();
+                    if selected.is_selected(&serial.set.port_name) {
+                        draw_flow_control_selector(ui, &mut serial);
+                        draw_parity_selector(ui, &mut serial);
+                        draw_stop_bits_selector(ui, &mut serial);
+                        draw_data_bits_selector(ui, &mut serial);
+                        draw_baud_rate_selector(ui, &mut serial);
+                    }
                 }
-            }
-        }
-    }
-}
-
-fn serial_window(mut commands: Commands, mut serials: Query<&mut Serials>) {
-    let mut serials = serials.single_mut();
-    for serial in serials.serial.iter_mut() {
-        let mut serial = serial.lock().unwrap();
-        if serial.is_open() {
-            if let None = serial.window() {
-                let window_id = commands
-                    .spawn(Window {
-                        title: serial.set.port_name().to_owned(),
-                        resolution: WindowResolution::new(800.0, 600.0),
-                        present_mode: PresentMode::AutoVsync,
-                        ..Default::default()
-                    })
-                    .id();
-                // second window camera
-                let camera_id = commands
-                    .spawn((
-                        Camera3d::default(),
-                        Camera {
-                            target: RenderTarget::Window(WindowRef::Entity(window_id)),
-                            ..Default::default()
-                        },
-                        Transform::from_xyz(6.0, 0.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
-                    ))
-                    .id();
-                info!("{} window id: {}", serial.set.port_name(), window_id);
-                *serial.window() = Some(window_id);
-                *serial.camera() = Some(camera_id);
-            }
-        }
-    }
-}
-
-#[derive(Resource)]
-struct Flag {
-    flag: bool,
-}
-
-fn serial_window_ui(
-    mut commands: Commands,
-    mut egui_ctx: Query<&mut Serials>,
-    mut flag: ResMut<Flag>,
-    asset_server: Res<AssetServer>,
-) {
-    let mut serials = egui_ctx.single_mut();
-    for serial in serials.serial.iter_mut() {
-        let mut serial = serial.lock().unwrap();
-        if serial.camera().is_some() {
-            if flag.flag {
-                commands.spawn((
-                    Text::new("你好aaa"),
-                    TextFont {
-                        font: asset_server.load("fonts/STSong.ttf"),
-                        font_size: 100.0,
-                        ..default()
-                    },
-                    Transform::from_xyz(0.0, 0.0, 0.0),
-                    // Since we are using multiple cameras, we need to specify which camera UI should be rendered to
-                    TargetCamera(serial.camera().unwrap()),
-                ));
-
-                flag.flag = false;
-            }
-        }
-    }
+                ui.horizontal(|ui| {
+                    if selected.selected() != "" {
+                        ui.label("当前选中:");
+                        ui.label(selected.selected());
+                    }else {
+                        ui.label("当前未选中串口");
+                    }
+                });
+                ui.separator();
+            });
+        });
 }
--- src/serial_ui/ui.rs
+++ src/serial_ui/ui.rs
@@ -1,1 +1,252 @@
+use crate::serial::port::Serial;
+use crate::serial::*;
+use bevy::{
+    prelude::*,
+    render::camera::RenderTarget,
+    window::{PresentMode, WindowClosing, WindowRef, WindowResolution},
+};
+use bevy_egui::{EguiContext, EguiContexts, EguiPlugin, egui};
+use std::sync::MutexGuard;
+use tokio_serial::{DataBits, FlowControl, Parity, StopBits};
 
+#[derive(Resource)]
+pub struct Selected {
+    selected: String,
+}
+impl Default for Selected {
+    fn default() -> Self {
+        Self { selected: "".to_string() }
+    }
+}
+impl Selected {
+    pub fn is_selected(&self, port_name: &str) -> bool {
+        self.selected == port_name
+    }
+    pub fn select(&mut self, port_name: &str) {
+        self.selected = port_name.to_string();
+    }
+    pub fn selected(&self) -> &str {
+        &self.selected
+    }
+}
+
+/// draw baud rate selector
+pub fn draw_baud_rate_selector(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>) {
+    ui.horizontal(|ui| {
+        ui.label("波特率");
+        egui::ComboBox::from_id_salt(serial.set.port_name.clone() + "0")
+            .width(60f32)
+            .selected_text(serial.set.baud_rate().to_string())
+            .show_ui(ui, |ui| {
+                for baud_rate in port::COMMON_BAUD_RATES.iter() {
+                    ui.selectable_value(serial.set.baud_rate(), *baud_rate, baud_rate.to_string())
+                        .on_hover_text("选择正确的波特率");
+                }
+            });
+    });
+}
+
+/// draw data bits selector
+pub fn draw_data_bits_selector(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>) {
+    ui.horizontal(|ui| {
+        ui.label("数据位");
+        egui::ComboBox::from_id_salt(serial.set.port_name.clone() + "1")
+            .width(60f32)
+            .selected_text(serial.set.data_size().to_string())
+            .show_ui(ui, |ui| {
+                for bits in [
+                    DataBits::Five,
+                    DataBits::Six,
+                    DataBits::Seven,
+                    DataBits::Eight,
+                ] {
+                    ui.selectable_value(serial.set.data_size(), bits, format!("{}", bits));
+                }
+            });
+    });
+}
+
+/// draw stop bits selector
+pub fn draw_stop_bits_selector(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>) {
+    ui.horizontal(|ui| {
+        ui.label("停止位");
+        egui::ComboBox::from_id_salt(serial.set.port_name.clone() + "2")
+            .width(60f32)
+            .selected_text(serial.set.stop_bits().to_string())
+            .show_ui(ui, |ui| {
+                for bits in [StopBits::One, StopBits::Two] {
+                    ui.selectable_value(serial.set.stop_bits(), bits, format!("{}", bits));
+                }
+            });
+    });
+}
+
+/// draw flow control selector
+pub fn draw_flow_control_selector(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>) {
+    ui.horizontal(|ui| {
+        ui.label("流控    ");
+        egui::ComboBox::from_id_salt(serial.set.port_name.clone() + "3")
+            .width(60f32)
+            .selected_text(serial.set.flow_control().to_string())
+            .show_ui(ui, |ui| {
+                for flow in [
+                    FlowControl::None,
+                    FlowControl::Software,
+                    FlowControl::Hardware,
+                ] {
+                    ui.selectable_value(serial.set.flow_control(), flow, format!("{}", flow));
+                }
+            });
+    });
+}
+
+/// draw parity selector
+pub fn draw_parity_selector(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>) {
+    ui.horizontal(|ui| {
+        ui.label("校验    ");
+        egui::ComboBox::from_id_salt(serial.set.port_name.clone() + "4")
+            .width(60f32)
+            .selected_text(serial.set.parity().to_string())
+            .show_ui(ui, |ui| {
+                for parity in [Parity::None, Parity::Odd, Parity::Even] {
+                    ui.selectable_value(serial.set.parity(), parity, format!("{}", parity));
+                }
+            });
+    });
+}
+
+pub fn open_ui(ui: &mut egui::Ui, serial: &mut MutexGuard<'_, Serial>, commands: &mut Commands, selected: &mut Selected) {
+    if serial.is_close() {
+        if ui.button("打开").clicked() {
+            selected.select(&serial.set.port_name);
+            info!("Open port {}", serial.set.port_name);
+            if let Some(tx) = serial.tx_channel() {
+                match tx.send(port::PortChannelData::PortOpen) {
+                    Ok(_) => {
+                        info!("Send open port message");
+                    }
+                    Err(e) => error!("Failed to open port: {}", e),
+                }
+                let time = chrono::Local::now().format("%Y%m%d_%H%M%S").to_string();
+                let port_name = serial.set.port_name.clone();
+                let file_name = format!("{}_{}.txt", port_name, time);
+                serial.data().add_source_file(file_name);
+            }
+        }
+    } else if serial.is_open() {
+        if ui.button("关闭").clicked() {
+            selected.select(&serial.set.port_name);
+            info!("关闭串口 {}", serial.set.port_name);
+            let port_name = serial.set.port_name.clone();
+            match serial.window() {
+                Some(window) => {
+                    commands.entity(window.clone()).despawn_recursive();
+                }
+                None => {}
+            };
+
+            if let Some(tx) = serial.tx_channel() {
+                match tx.send(port::PortChannelData::PortClose(port_name)) {
+                    Ok(_) => {
+                        info!("Send close port message");
+                    }
+                    Err(e) => error!("Failed to close port: {}", e),
+                }
+            }
+        }
+    }
+}
+
+pub fn close_event_system(
+    mut window_close_events: EventReader<WindowClosing>,
+    mut serials: Query<&mut Serials>,
+) {
+    for event in window_close_events.read() {
+        let mut serial = serials.get_single_mut().unwrap();
+        for serial in serial.serial.iter_mut() {
+            let mut serial = serial.lock().unwrap();
+            let port_name = serial.set.port_name.clone();
+            if serial.is_open() {
+                if let Some(window) = serial.window() {
+                    if *window == event.window {
+                        if let Some(tx) = serial.tx_channel() {
+                            match tx.send(port::PortChannelData::PortClose(port_name)) {
+                                Ok(_) => {
+                                    info!("Send close port message");
+                                }
+                                Err(e) => error!("Failed to close port: {}", e),
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+pub fn serial_window(mut commands: Commands, mut serials: Query<&mut Serials>) {
+    let mut serials = serials.single_mut();
+    for serial in serials.serial.iter_mut() {
+        let mut serial = serial.lock().unwrap();
+        if serial.is_open() {
+            if let None = serial.window() {
+                let window_id = commands
+                    .spawn(Window {
+                        title: serial.set.port_name().to_owned(),
+                        resolution: WindowResolution::new(800.0, 600.0),
+                        present_mode: PresentMode::AutoVsync,
+                        ..Default::default()
+                    })
+                    .id();
+                // second window camera
+                let camera_id = commands
+                    .spawn((
+                        Camera3d::default(),
+                        Camera {
+                            target: RenderTarget::Window(WindowRef::Entity(window_id)),
+                            ..Default::default()
+                        },
+                        Transform::from_xyz(6.0, 0.0, 0.0).looking_at(Vec3::ZERO, Vec3::Y),
+                    ))
+                    .id();
+                info!("{} window id: {}", serial.set.port_name(), window_id);
+                *serial.window() = Some(window_id);
+                *serial.camera() = Some(camera_id);
+            }
+        }
+    }
+}
+
+#[derive(Resource)]
+pub struct Flag {
+    pub flag: bool,
+}
+
+pub fn serial_window_ui(
+    mut commands: Commands,
+    mut egui_ctx: Query<&mut Serials>,
+    mut flag: ResMut<Flag>,
+    asset_server: Res<AssetServer>,
+) {
+    let mut serials = egui_ctx.single_mut();
+    for serial in serials.serial.iter_mut() {
+        let mut serial = serial.lock().unwrap();
+        if serial.camera().is_some() {
+            if flag.flag {
+                commands.spawn((
+                    Text::new("你好aaa"),
+                    TextFont {
+                        font: asset_server.load("fonts/STSong.ttf"),
+                        font_size: 100.0,
+                        ..default()
+                    },
+                    Transform::from_xyz(0.0, 0.0, 0.0),
+                    // Since we are using multiple cameras, we need to specify which camera UI should be rendered to
+                    TargetCamera(serial.camera().unwrap()),
+                ));
+
+                flag.flag = false;
+            }
+        }
+    }
+}
